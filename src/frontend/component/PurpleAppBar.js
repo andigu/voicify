3396fd9939eaff02064d259df917180a77a034a7
Andi Gu <andi.gu.ca@gmail.com>
2017-10-07 20:56:53 -0400

    Initial commit

---
+++ src/frontend/component/PurpleAppBar.js
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react';
+import { AppBar } from 'react-toolbox/lib/app_bar';
+import Logo from './Logo.js';
+import theme from './PurpleAppBar.css';
+
+const PurpleAppBar = ({ children, ...other }) => (
+  <AppBar {...other} theme={theme}>
+    <Logo /> App Example
+    {children}
+  </AppBar>
+);
+
+export default PurpleAppBar;
---
+++ src/frontend/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './component/App';
+import {AppContainer} from 'react-hot-loader';
+
+const rootEl = document.getElementById('app');
+
+const render = () => {
+    ReactDOM.render(
+        <AppContainer>
+            <App/>
+        </AppContainer>,
+        rootEl
+    );
+};
+
+
+render();
