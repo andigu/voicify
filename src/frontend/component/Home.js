d7e0d3720076afe95dd885d60e7d1fb4c7d2c43e
Andi Gu <andi.gu.ca@gmail.com>
2017-10-10 21:39:47 -0400

    WOrked on home page

--- src/frontend/component/Home.js
+++ src/frontend/component/Home.js
@@ -1,15 +1,22 @@
 import React, {Component} from 'react';
-import {Button} from 'react-toolbox/lib/button';
 import {connect} from 'react-redux';
-import {encodeQueryData} from '../lib/uri';
-import styled from 'styled-components';
-import {SpotifyLoginButton} from './SpotifyLoginButton';
 import {namespaces, stateSelector} from '../redux/index';
 import {Redirect} from 'react-router';
+import styles from './App.css';
+import {SpotifyLoginButton} from './SpotifyLoginButton';
 
 @connect(stateSelector(namespaces.spotify))
 export class Home extends Component {
     render() {
-        return this.props.state.spotify.accessToken === null ? <SpotifyLoginButton/> : <Redirect to="/app"/>
+        if (this.props.state.spotify.accessToken !== null) {
+            return <Redirect to="/app"/>;
+        } else {
+            return <div className={styles.container}>
+                <h1 className={styles.header}>Voicify.</h1>
+                <div className={styles.loginbtn}>
+                <SpotifyLoginButton />
+                </div>
+            </div>;
+        }
     }
 }
